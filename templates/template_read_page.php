e156ebfbb6f67237594beddb1dc77065dd363497
m-a-hannan <gr981888@gmail.com>
2026-01-18 19:55:58 +0600

    Add CRUD page templates

---
+++ templates/template_read_page.php
@@ -0,0 +1,27 @@
+<?php
+require_once dirname(__DIR__) . '/include/config.php';
+?>
+<?php include(ROOT_PATH . '/include/header_resources.php') ?>
+<?php include(ROOT_PATH . '/include/header.php') ?>
+<?php include(ROOT_PATH . '/sidebar.php') ?>
+<!--begin::App Main-->
+<main class="app-main">
+	<!--begin::App Content-->
+	<div class="app-content">
+		<!--begin::Container-->
+		<div class="container-fluid">
+			<!--begin::Row-->
+			<div class="row">
+				<div class="container py-5">
+					<!-- Content title -->
+					<h1>Copy This page to Create New Read/View Page</h1>
+					<!-- Add Contents below -->
+				</div>
+			</div>
+			<!-- end::Row -->
+		</div>
+	</div>
+</main>
+<!--end::App Main-->
+<?php include(ROOT_PATH . '/include/footer.php') ?>
+<?php include(ROOT_PATH . '/include/footer_resources.php') ?>
